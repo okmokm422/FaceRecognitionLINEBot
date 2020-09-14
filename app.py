8be83dc82cb116661f8466de546dd94f8a35443e
okmokm422 <okmokm422@gmail.com>
2020-09-14 19:06:31 +0900

    20200914

--- app.py
+++ app.py
@@ -17,14 +17,29 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, ImageMessage # 追記
 )
 
+# バイナリデータとして画像を扱う
+from io import BytesIO
+
+# AzureのPython SDKライブラリ
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+
 app = Flask(__name__)
 
 # 環境変数からトークンを読み込み
+# LINE
 YOUR_CHANNEL_ACCESS_TOKEN = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN')
 YOUR_CHANNEL_SECRET = os.getenv('YOUR_CHANNEL_SECRET')
-
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+# LINE
+YOUR_FACE_API_KEY = os.environ["YOUR_FACE_API_KEY"]
+YOUR_FACE_API_ENDPOINT = os.environ["YOUR_FACE_API_ENDPOINT"]
+face_client = FaceClient(
+    YOUR_FACE_API_ENDPOINT,
+    CognitiveServicesCredentials(YOUR_FACE_API_KEY)
+    )
 
 
 # 後述のwebhook通信をLINEチャネルから受け取るためのエンドポイントを設定
@@ -60,10 +75,32 @@ def handle_message(event):
 # 画像メッセージのときの挙動
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
+    try:
+        # メッセージIDを受け取る
+        message_id = event.message.id
+        # メッセージIdに含まれるmassage_contentを抽出する
+        message_content = line_bot_api.get_message_content(message_id)
+        # contentの画像データをバイナリデータとして扱えるようにする
+        image = BytesIO(message_content.content)
+        # Detect from streamで顔検出
+        detected_faces = face_client.face.detect_with_stream(image)
+        print(detected_faces)
+
+        # 検出結果に応じて処理を分ける
+        if detected_faces != []:
+            # 検出された顔の最初のIDを取得
+            text = detected_faces[0].face_id
+        else:
+            # 検出されない場合のメッセージ
+            text = 'no faces detected'
+    except:
+        # エラー時のメッセージ
+        text = 'error'
+    
     # LINEチャネルを通じてメッセージを応答
     line_bot_api.reply_message(
         event.reply_token, 
-        TextSendMessage(text='画像です')
+        TextSendMessage(text=text)
     )
 
 # app.pyがメインスコープとして呼ばれた際には、appを起動
